e7542ca715bb0bbe4dc0eeb9f1497523444c9065
HexPandaa <47880094+HexPandaa@users.noreply.github.com>
2021-01-13 17:27:14 +0100

    Make sure the number of seconds if positive, just in case

--- cogs/calendar.py
+++ cogs/calendar.py
@@ -107,7 +107,7 @@ class CalendarCog(commands.Cog):
         try:
             while len(self.reacted) != len(self.students):
                 timeout = event.end.shift(minutes=+15) - now(config.TIMEZONE)
-                reaction, user = await self.bot.wait_for('reaction_add', timeout=timeout.seconds, check=check)
+                reaction, user = await self.bot.wait_for('reaction_add', timeout=abs(timeout.seconds), check=check)
                 task = self.bot.loop.create_task(self.check_in(user, courses, event, bot_message))
 
         except asyncio.TimeoutError:
